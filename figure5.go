199de754d502abaaa4948c901f1e3b7b98cdd2e1
Bryan C. Mills <bcmills@google.com>
2023-09-21 14:15:42 -0400

    figure5: correct figure5.go to refer to Figure 5
    Move the previous figure5.go, which actually referred to figure 7, to
    figure7.go.

--- figure5.go
+++ figure5.go
@@ -1,4 +1,4 @@
-// Copyright 2020 The Go Authors. All rights reserved.
+// Copyright 2023 The Go Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
@@ -6,99 +6,39 @@
 
 // figure5 illustrates figure 5 of Featherweight Go.
 //
-// Unfortunately, it does not compile:
-//
-//	./figure5.go:96:6: invalid recursive type: Edge refers to itself
-//	./figure5.go:96:6: invalid recursive type Edge
-//	        ./figure5.go:96:6: Edge refers to
-//	        ./figure5.go:100:6: Vertex refers to
-//	        ./figure5.go:96:6: Edge
-//	./figure5.go:100:6: invalid recursive type: Vertex refers to itself
+// Unfortunately, it does not compile because the interface Eq
+// constrains its own type parameter.
 package main
 
-// Featherweight Go, Fig. 3
+import "fmt"
 
-type Function[a any, b any] interface {
-	Apply(x a) b
-}
-type incr struct{ n int }
+// Featherweight Go, Fig. 5
 
-func (this incr) Apply(x int) int {
-	return x + this.n
+// Eq is the Eq interface from figure 5.
+type Eq[a Eq[a]] interface {
+	Equal(that a) bool
 }
 
-type pos struct{}
-
-func (this pos) Apply(x int) bool {
-	return x > 0
-}
-
-type compose[a any, b any, c any] struct {
-	f Function[a, b]
-	g Function[b, c]
-}
-
-func (this compose[a, b, c]) Apply(x a) c {
-	return this.g.Apply(this.f.Apply(x))
-}
-
-// Adapted from Featherweight Go, Fig. 4
-
-type Eq[a any] interface {
-	Equal(a) bool
-}
 type Int int
 
 func (this Int) Equal(that Int) bool {
 	return this == that
 }
 
-type List[a any] interface {
-	Match(casenil Function[Nil[a], any], casecons Function[Cons[a], any]) any
-}
-type Nil[a any] struct{}
-
-func (xs Nil[a]) Match(casenil Function[Nil[a], any], casecons Function[Cons[a], any]) any {
-	return casenil.Apply(xs)
-}
-
-type Cons[a any] struct {
-	Head a
-	Tail List[a]
+// Pair is the Pair struct from figure 5, but with the type constraints
+// strengthened to Eq because the final Go design for type parameters
+// does not allow methods that refine the receiver's type constraints.
+type Pair[a Eq, b Eq] struct {
+	left  a
+	right b
 }
 
-func (xs Cons[a]) Match(casenil Function[Nil[a], any], casecons Function[Cons[a], any]) any {
-	return casecons.Apply(xs)
+func (this Pair[a, b]) Equal(that Pair[a, b]) bool {
+	return this.left.Equal(that.left) && this.right.Equal(that.right)
 }
 
-type lists[a any, b any] struct{}
-
-func (_ lists[a, b]) Map(f Function[a, b], xs List[a]) List[b] {
-	return xs.Match(mapNil[a, b]{}, mapCons[a, b]{f}).(List[b])
+func main() {
+	var i, j Int = 1, 2
+	var p Pair[Int, Int] = Pair[Int, Int]{i, j}
+	fmt.Println(p.Equal(p)) // true
 }
-
-type mapNil[a any, b any] struct{}
-
-func (m mapNil[a, b]) Apply(_ Nil[a]) any {
-	return Nil[b]{}
-}
-
-type mapCons[a any, b any] struct {
-	f Function[a, b]
-}
-
-func (m mapCons[a, b]) Apply(xs Cons[a]) any {
-	return Cons[b]{m.f.Apply(xs.Head), lists[a, b]{}.Map(m.f, xs.Tail)}
-}
-
-// Featherweight Go, Fig. 5
-
-type Edge[e Edge[e, v], v Vertex[e, v]] interface {
-	Source() v
-	Target() v
-}
-type Vertex[e Edge[e, v], v Vertex[e, v]] interface {
-	Edges() List[e]
-}
-
-func main() {}
